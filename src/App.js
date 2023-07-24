11fed86d2bd8fa7839f0c401a7f96eb62353df0f
towhid-siddiqui <towsdk@gmail.com>
2023-07-24 13:33:26 +0600

    tic-tac-toe added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Tictactoe from './Tictactoe';
+
+function App() {
+  return (
+    <div className="App">
+     <Tictactoe />
+    </div>
+  );
+}
+
+export default App;
